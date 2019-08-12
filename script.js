22120d161eba0ec2b9776dbb29fc1b7751dca55a
Morgan Reilly <morgan.reilly27@gmail.com>
2019-08-12 16:55:09 +0100

    Evaluating model

--- script.js
+++ script.js
@@ -51,6 +51,10 @@ async function run() {
   tfvis.show.modelSummary({name: 'Model Architecture'}, model);
   
   await train(model, data);
+
+  // Displaying evaluation
+  await showAccuracy(model, data);
+  await showConfusion(model, data);
 }
 
 document.addEventListener('DOMContentLoaded', run);
@@ -171,4 +175,54 @@ async function train(model, data) {
       shuffle: true,
       callbacks: fitCallbacks
     });
+}
+
+/**
+ * Evaluating the model by:
+ * 
+ * Making Predictions
+ * Computing accuracy metrics
+ * Showing the metrics
+ */
+const classNames = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
+
+/**
+ * Make predictions
+ * Take 500 images and predict what digit is in them
+ */
+function doPrediction(model, data, testDataSize = 500) {
+  const IMAGE_WIDTH = 28;
+  const IMAGE_HEIGHT = 28;
+  const testData = data.nextTestBatch(testDataSize);
+  const testxs = testData.xs.reshape([testDataSize, IMAGE_WIDTH, IMAGE_HEIGHT, 1]);
+  const labels = testData.labels.argMax([-1]);
+  const preds = model.predict(testxs).argMax([-1]);
+
+  testxs.dispose();
+  return [preds, labels];
+}
+
+/**
+ * Show per class accuracy
+ */
+async function showAccuracy(model, data) {
+  const [preds, labels] = doPrediction(model, data);
+  const classAccuracy = await tfvis.metrics.perClassAccuracy(labels, preds);
+  const container = {name: 'Accuracy', tab: 'Evaluation'};
+  tfvis.show.perClassAccuracy(container, classAccuracy, classNames);
+
+  labels.dispose();
+}
+
+/**
+ * Show confusion matrix
+ */
+async function showConfusion(model, data) {
+  const [preds, labels] = doPrediction(model, data);
+  const confusionMatrix = await tfvis.metrics.confusionMatrix(labels, preds);
+  const container = {name: 'Confusion Matrix', tab: 'Evaluation'};
+  tfvis.render.confusionMatrix(
+      container, {values: confusionMatrix}, classNames);
+
+  labels.dispose();
 }
